08d9f651b7c23a05e883694412f259139d9eba7b
kuldeep singh <108728408+12ksingh@users.noreply.github.com>
2025-05-28 23:52:03 +0530

    Delete src directory

--- src/Components/ProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from 'react-router-dom';
-
-const ProtectedRoute = ({ children }) => {
-  const isAuthenticated = localStorage.getItem('auth') === 'true';
-
-  return isAuthenticated ? children : <Navigate to="/Login" />;
-};
-
-export default ProtectedRoute;
